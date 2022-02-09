8fbc63eded258e2c6f10e00f450c7c75cef95edc
hoseindig <hosein_dig@yahoo.com>
2022-02-09 17:21:37 +0330

    add navbar

--- src/home.jsx
+++ src/home.jsx
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
-
+import NavbarBox from "./components/navbar";
 class HomePage extends Component {
   state = {};
   render() {
     return (
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <React.Fragment>
+        <NavbarBox />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+      </React.Fragment>
     );
   }
 }
