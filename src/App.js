f6b5da30b9b20588e40361d9f1439daf1b280e0a
hoseindig <hosein_dig@yahoo.com>
2022-02-09 17:20:55 +0330

    add navbar

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.css";
-
+import "react-toastify/dist/ReactToastify.css";
 import HomePage from "./home";
 import Login from "./components/login";
 function App() {
