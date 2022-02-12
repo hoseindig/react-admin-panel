cc4309b7100d4dad882d3cb3221407ba20138aba
hoseindig <hosein_dig@yahoo.com>
2022-02-12 18:00:10 +0330

    8  chart with fake data

--- src/home.jsx
+++ src/home.jsx
@@ -6,8 +6,15 @@ import CardBox from "./components/card";
 import { Col, Row } from "react-bootstrap";
 
 import BarChartBox from "./components/chart/barChart";
-import LineChartBox from './components/chart/LineChart';
-
+import LineChartBox from "./components/chart/LineChart";
+import BarChartWithCustomizedEvent from "./components/chart/BarChartWithCustomizedEvent";
+import BarChartNoPadding from "./components/chart/BarChartNoPadding";
+import PieChartWithCustomizedLabel from "./components/chart/PieChartWithCustomizedLabel";
+// import CustomContentOfTooltip from "./components/chart/CustomContentOfTooltip";
+// imprt TwoLevelPieChart from "./components/chart/TwoLevelPieChart";
+import TwoSimplePieChart from "./components/chart/TwoSimplePieChart";
+// mport BrushBarChart from "./components/chart/BrushBarChart";
+import AreaChartConnectNulls from "./components/chart/AreaChartConnectNulls";
 class HomePage extends Component {
   state = {};
   render() {
@@ -33,9 +40,25 @@ class HomePage extends Component {
           <Col md={4} sm={4} xs={12}>
             <BarChartBox />
           </Col>
-          <Col md={4} sm={4} xs={12}></Col>
+          <Col md={4} sm={4} xs={12}>
+            <BarChartWithCustomizedEvent />
+          </Col>
           <Col md={4} sm={4} xs={12}>
             <LineChartBox />
+          </Col>
+        </Row>
+         <Row>
+          <Col md={2} sm={4} xs={12}>
+            <TwoSimplePieChart />
+          </Col>
+          <Col md={2} sm={4} xs={12}>
+            <TwoSimplePieChart />
+          </Col>
+          <Col md={4} sm={4} xs={12}>
+            <BarChartNoPadding />
+          </Col>
+          <Col md={4} sm={4} xs={12}>
+            <AreaChartConnectNulls />
           </Col>
         </Row>
 
